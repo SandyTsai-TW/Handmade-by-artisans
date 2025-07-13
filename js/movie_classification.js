d2d077cd9ab3cf043ae9a2881b5d4466ed7d8a35
SandyTsai <yume3101@gmail.com>
2025-07-13 16:22:09 +0800

    keyword.html commit

--- js/movie_classification.js
+++ js/movie_classification.js
@@ -19,4 +19,32 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
+// keyword.html
+var swiper = new Swiper(".mySwiper-1", {
+  slidesPerView: 3.5,
+  spaceBetween: 2,
+  scrollbar: {
+    el: ".swiper-scrollbar-1",
+    draggable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2.8,
+      spaceBetween: 2,
+    },
+    375: {
+      slidesPerView: 3.5,
+      spaceBetween: 2,
+    },
+    768: {
+      slidesPerView: 4.5,
+      spaceBetween: 2,
+    },
+    1024: {
+      slidesPerView: 5.5,
+      spaceBetween: 6,
+    },
+  },
+});
+
 
