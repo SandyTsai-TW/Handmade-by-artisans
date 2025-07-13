d070186abf90a6a2de7f0ec8729834938e49aac9
SandyTsai <yume3101@gmail.com>
2025-07-13 16:54:15 +0800

    keyword.js commit

--- js/keyword.js
+++ js/keyword.js
@@ -1,19 +1,27 @@
-// movieUnder-swiper
+// keyword.html
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4.5,
+  slidesPerView: 3.5,
   spaceBetween: 2,
   scrollbar: {
     el: ".swiper-scrollbar",
     draggable: true,
   },
   breakpoints: {
+    320: {
+      slidesPerView: 2.8,
+      spaceBetween: 2,
+    },
+    375: {
+      slidesPerView: 3.5,
+      spaceBetween: 2,
+    },
     768: {
       slidesPerView: 4.5,
       spaceBetween: 2,
     },
     1024: {
       slidesPerView: 5.5,
-      spaceBetween: 2,
+      spaceBetween: 6,
     },
   },
 });
