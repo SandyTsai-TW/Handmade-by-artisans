b376475286c79f05241e0518b36a92ae9b0e917b
SandyTsai <yume3101@gmail.com>
2025-07-15 18:36:21 +0800

    video js commit

--- js/video_content.js
+++ js/video_content.js
@@ -18,3 +18,20 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+const click111 = document.querySelector("#click111");
+const open111 = document.querySelector("#open111");
+
+const keyframes = {
+  translate: [0,'0 -46%']
+}
+const options = {
+  duration: 2000,
+  easing: "ease",
+  fill: "backward",
+};
+
+click111.addEventListener('click',() => {
+  // console.log("open");
+  open111.animate(keyframes, options)
+});
