57263d77aba7642ca67a10d4aab8384288a155f9
SandyTsai <yume3101@gmail.com>
2025-07-23 00:48:17 +0800

    checkout_1 switch commit

--- js/checkout_1.js
+++ js/checkout_1.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
-
+// card
   $(".flip").click(function () {
     $("#arrow").toggleClass("add");
+  });
+
+// switch
+  $(".mySwitch").click(function () {
+    $(".switch-bg").toggleClass("bg-add"),
+    $(".circle").toggleClass("add");
   });
 
 });
